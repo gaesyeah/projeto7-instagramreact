3c47a67da71993056b2aedd667ad44d0f366c565
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-05-03 00:03:32 -0300

    hotfix para futuro uso do map nas sugestões

--- src/index.js
+++ src/index.js
@@ -240,74 +240,76 @@ function App() {
                             <p class="text_3">Sugestões para você</p>
                             <p class="text_3 text_ver">Ver tudo</p>
                         </div>
-                        <div class="others">
-                            <div class="left_part">
-                                <img class="img_margin_others" src="./assets/storie_generico.png" />
-                                <div class="text_part">
-                                    <div class="text_part_column">
-                                        <p class="text_bold">nome genérico</p>
-                                        <p class="text_2">segue você</p>
+                        <div>
+                            <div class="others">
+                                <div class="left_part">
+                                    <img class="img_margin_others" src="./assets/storie_generico.png" />
+                                    <div class="text_part">
+                                        <div class="text_part_column">
+                                            <p class="text_bold">nome genérico</p>
+                                            <p class="text_2">segue você</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="right_part">
-                                <p class="text_3 text_seguir">seguir</p>
-                            </div>
-                        </div>
-                        <div class="others">
-                            <div class="left_part">
-                                <img class="img_margin_others" src="./assets/storie_generico.png" />
-                                <div class="text_part">
-                                    <div class="text_part_column">
-                                        <p class="text_bold">nome genérico</p>
-                                        <p class="text_2">segue você</p>
-                                    </div>
+                                <div class="right_part">
+                                    <p class="text_3 text_seguir">seguir</p>
                                 </div>
                             </div>
-                            <div class="right_part">
-                                <p class="text_3 text_seguir">seguir</p>
-                            </div>
-                        </div>
-                        <div class="others">
-                            <div class="left_part">
-                                <img class="img_margin_others" src="./assets/storie_generico.png" />
-                                <div class="text_part">
-                                    <div class="text_part_column">
-                                        <p class="text_bold">nome genérico</p>
-                                        <p class="text_2">segue você</p>
+                            <div class="others">
+                                <div class="left_part">
+                                    <img class="img_margin_others" src="./assets/storie_generico.png" />
+                                    <div class="text_part">
+                                        <div class="text_part_column">
+                                            <p class="text_bold">nome genérico</p>
+                                            <p class="text_2">segue você</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="right_part">
-                                <p class="text_3 text_seguir">seguir</p>
-                            </div>
-                        </div>
-                        <div class="others">
-                            <div class="left_part">
-                                <img class="img_margin_others" src="./assets/storie_generico.png" />
-                                <div class="text_part">
-                                    <div class="text_part_column">
-                                        <p class="text_bold">nome genérico</p>
-                                        <p class="text_2">segue você</p>
-                                    </div>
+                                <div class="right_part">
+                                    <p class="text_3 text_seguir">seguir</p>
                                 </div>
                             </div>
-                            <div class="right_part">
-                                <p class="text_3 text_seguir">seguir</p>
-                            </div>
-                        </div>
-                        <div class="others">
-                            <div class="left_part">
-                                <img class="img_margin_others" src="./assets/storie_generico.png" />
-                                <div class="text_part">
-                                    <div class="text_part_column">
-                                        <p class="text_bold">nome genérico</p>
-                                        <p class="text_2">segue você</p>
+                            <div class="others">
+                                <div class="left_part">
+                                    <img class="img_margin_others" src="./assets/storie_generico.png" />
+                                    <div class="text_part">
+                                        <div class="text_part_column">
+                                            <p class="text_bold">nome genérico</p>
+                                            <p class="text_2">segue você</p>
+                                        </div>
                                     </div>
                                 </div>
+                                <div class="right_part">
+                                    <p class="text_3 text_seguir">seguir</p>
+                                </div>
                             </div>
-                            <div class="right_part">
-                                <p class="text_3 text_seguir">seguir</p>
+                            <div class="others">
+                                <div class="left_part">
+                                    <img class="img_margin_others" src="./assets/storie_generico.png" />
+                                    <div class="text_part">
+                                        <div class="text_part_column">
+                                            <p class="text_bold">nome genérico</p>
+                                            <p class="text_2">segue você</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="right_part">
+                                    <p class="text_3 text_seguir">seguir</p>
+                                </div>
+                            </div>
+                            <div class="others">
+                                <div class="left_part">
+                                    <img class="img_margin_others" src="./assets/storie_generico.png" />
+                                    <div class="text_part">
+                                        <div class="text_part_column">
+                                            <p class="text_bold">nome genérico</p>
+                                            <p class="text_2">segue você</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="right_part">
+                                    <p class="text_3 text_seguir">seguir</p>
+                                </div>
                             </div>
                         </div>
                         <div class="desc">
