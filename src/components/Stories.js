c073e9fe5befffa609a8ef82b907afa02ba9e1a9
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-05-03 13:14:01 -0300

    stories sendo gerados com map

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,57 +1,26 @@
 export default function Stories() {
+
+    function story () {
+        return (
+            <div class="storie_div">
+                <img class="storie_size" src="./assets/stories_background.jpg" />
+                <img class="storie_img_size" src="./assets/storie_generico.png" />
+                <p>Storie genérico</p>
+            </div>
+        );
+    }
+
+    const array_stories = [];
+    for (let i = 0; i < 8; i++) {
+        array_stories.push(story());
+    }
+
+
     return (
         <div class="stories">
 
             <div class="stories_no_arrow">
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie1</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie2</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie3</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie4</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie5</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie6</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie7</p>
-                </div>
-
-                <div class="storie_div">
-                    <img class="storie_size" src="./assets/stories_background.jpg" />
-                    <img class="storie_img_size" src="./assets/storie_generico.png" />
-                    <p>storie8</p>
-                </div>
-            
+                {array_stories.map(i => i)}
             </div>
             
             <ion-icon class="arrow" name="chevron-forward-circle"></ion-icon>
