c116c62309a8447970a8b554a0bfc4f70196aebf
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-05-05 18:55:09 -0300

    alteração na semantica do verificador de like do usuario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,20 +4,20 @@ export default function Post(props) {
     const {user_img, user, image, user_comment, N_likes, comment_img, title, N_comments, comment1, comment2, comment3} = props.dataPost;
     
     let [savePost, setSavePost] = useState('bookmark-outline');
-    let [counterLikes, setCounterLikes] = useState(0);
+    let [userLike, setUserLike] = useState('not');
     let [likes, setLikes] = useState(N_likes);
     let [iconLikes, setIconLikes] = useState('heart-outline');
     let [styleLikes, setStyleLikes] = useState('black');
 
     function changeLikes() {
-        if (counterLikes === 0) {
-            setCounterLikes(1);
+        if (userLike === 'not') {
+            setUserLike('yes');
 
             setLikes(likes + 1);
             setIconLikes('heart');
             setStyleLikes('red');
         } else {
-            setCounterLikes(0);
+            setUserLike('not');
 
             setLikes(likes - 1);
             setIconLikes('heart-outline');
@@ -37,7 +37,7 @@ export default function Post(props) {
                 </div>
             </div>
             <img
-                onClick={() => {counterLikes === 0 ? changeLikes() : undefined}}
+                onClick={() => {userLike === 'not' ? changeLikes() : undefined}}
                 class="post_img_size"
                 src={image}
             />
